41b3941d6a70d15b5702fa4fa5e43d8554bdad46
Rithwick08 <reddyrithwick@gmail.com>
2025-05-14 19:08:45 +0530

    done

--- Assignment7.js
+++ Assignment7.js
@@ -1,14 +1,31 @@
-const calc=()=>
-{
-    var n1=document.getElementById('n1').value;
-    var n2=document.getElementById('n2').value;
-    var sum=parseFloat(n1)+parseFloat(n2);
-    var sub=parseFloat(n1)-parseFloat(n2);
-    var mul=parseFloat(n1)*parseFloat(n2);
-     var div=parseFloat(n1)/parseFloat(n2);
-     document.getElementById('res').innerHTML = 
-        "Sum is: " + sum + "<br>" +
-        "Sub is: " + sub + "<br>" +
-        "Mul is: " + mul + "<br>" +
-        "Div is: " + div;
+const add = () => {
+    var n1 = document.getElementById('n1').value;
+    var n2 = document.getElementById('n2').value;
+    var sum = parseFloat(n1) + parseFloat(n2);
+    document.getElementById('res').innerHTML = "Sum is: " + sum;
+}
+
+const sub = () => {
+    var n1 = document.getElementById('n1').value;
+    var n2 = document.getElementById('n2').value;
+    var sum = parseFloat(n1) - parseFloat(n2);
+    document.getElementById('res').innerHTML = "Sub is: " + sum;
+}
+
+const mul = () => {
+    var n1 = document.getElementById('n1').value;
+    var n2 = document.getElementById('n2').value;
+    var sum = parseFloat(n1) * parseFloat(n2);
+    document.getElementById('res').innerHTML = "Mul is: " + sum;
+}
+
+const div = () => {
+    var n1 = document.getElementById('n1').value;
+    var n2 = document.getElementById('n2').value;
+    if (parseFloat(n2) === 0) {
+        document.getElementById('res').innerHTML = "Cannot divide by zero";
+    } else {
+        var sum = parseFloat(n1) / parseFloat(n2);
+        document.getElementById('res').innerHTML = "Div is: " + sum;
+    }
 }
